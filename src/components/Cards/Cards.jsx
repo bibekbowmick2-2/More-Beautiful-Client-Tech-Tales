26ce55981a0bda616c24d1b8a2d808e178d7851f
unknown <bibektotol@gmail.com>
2025-02-13 03:40:29 +0600

    fixed navbar and fixed height card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,10 +11,10 @@ const Cards = ({ games }) => {
 
   // const {name,description,picture,ratin} = games;
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-2 py-10 bg-[#04020e] ">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-2 py-10 bg-[#080325] ">
       {games.map((game) => (
         <div className="mx-auto w-full pt-5">
-          <div className="card bg-[#3f3a5b70] w-11/12 mx-auto shadow-xl rounded-none ">
+          <div className="card bg-[#3f3a5b70] w-11/12 mx-auto shadow-xl rounded-none h-[550px] ">
             <div className="w-[100px] h-[100px]  mx-auto">
               <PhotoProvider>
                 <PhotoView src={game?.image}>
@@ -23,14 +23,14 @@ const Cards = ({ games }) => {
               </PhotoProvider>
              
             </div>
-            <div className="card-body">
+            <div className="card-body overflow-y-auto">
               <h2 className="card-title">{game?.title}</h2>
               <p>{game?.longDescription}</p>
               <p> {game?.category}</p>
 
               <div className="card-actions justify-start">
                 <Link to={`/product-details/${game.title}`}>
-                  <button className="btn btn-accent">Explore Details</button>
+                  <button className="btn btn-accent">See More</button>
                 </Link>
                 <br />
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -74,8 +74,8 @@ const Header = () => {
     </>
   );
   return (
-    <div className="navbar bg-[#080325] text-white  ">
-      <div className="navbar-start">
+    <div className="navbar bg-[#080325] text-white fixed z-10 ">
+      <div className="navbar-start ">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
